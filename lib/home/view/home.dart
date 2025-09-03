eb534a8192e9e2bb96440db11ab4a7efc2bcbd7f
Gene <geneyllanes@gmail.com>
2025-09-03 12:07:58 -0400

    Update home.dart

--- lib/home/view/home.dart
+++ lib/home/view/home.dart
@@ -30,7 +30,7 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     const id = 'DeRosa';
-    const title = 'July 26th - Aug 2';
+    const title = 'Aug 24 - Aug 30';
     const price = '399';
     final timeStamp = DateTime.now().toString();
 
